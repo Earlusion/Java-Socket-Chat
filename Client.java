12ec8fe76a27dd584c96fee1ca294b2170df4a6a
Marko <marko_XLR8@hotmail.com>
2019-03-27 00:42:31 -0400

    Used template from Baeldung site to tweak server and client methods. Server still isn't receiving strings properly from client.

--- Client.java
+++ Client.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class Client {
     String name;
-    Socket sock;
-    DataOutputStream send;
-    DataInputStream receive;
+
+    private static Socket sock;
+    private static PrintWriter out;
+    private static BufferedReader in;
 
     public Client(String newName) {
         this.name = newName;
@@ -20,29 +21,44 @@ public class Client {
         String str = "";
 
         str += name + " has entered the chat";
+
         return str;
+    }
+ 
+    public static void startConnection(String ip, int port) throws IOException {
+        sock = new Socket(ip, port);
+        out = new PrintWriter(sock.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+    }
+ 
+    public String outMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+ 
+    public static void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        sock.close();
     }
 
     public static void main(String[] args) {
         try {
-            Socket sock = new Socket("127.0.0.1", 7000);
-            DataOutputStream send = new DataOutputStream(sock.getOutputStream());
-            DataInputStream receive = new DataInputStream(sock.getInputStream());
+            startConnection("127.0.0.1", 7000);
 
             Scanner input = new Scanner(System.in);
             System.out.print("Enter your name: ");
 
-            Client user = new Client(input.nextLine());
+            Client user = new Client(input.next());
+            user.outMessage(user.joined());
 
-            send.writeChars(user.joined());
-            send.flush();
+            System.out.print("Say something: ");
+            String temp = input.next();
 
-            do{
-                send.writeChars(input.nextLine());
-                send.flush();
-            }while(input.nextLine().equals("exit()"));
+            user.outMessage("temp");
 
-            sock.close();
+            stopConnection();
             input.close();
 
         } catch (IOException e) {
--- Server.java
+++ Server.java
@@ -3,27 +3,44 @@ import java.io.*;
 import java.util.*;
 
 public class Server {
+    static ServerSocket servSock;
+    static Socket sock;
+
+    static PrintWriter out;
+    static BufferedReader in;
+
     ArrayList<Client> global = new ArrayList<Client>();
 
-    public static void main(String[] args) {
+    public static void start(int port) {
         try {
-            ServerSocket servSock = new ServerSocket(7000);
+            servSock = new ServerSocket(port);
+            sock = servSock.accept();
+            out = new PrintWriter(sock.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 
-            DataOutputStream send = new DataOutputStream(sock.getOutputStream());
-            // DataInputStream receive = new DataInputStream(sock.getInputStream());
-
-            BufferedReader d = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-
-            String test = d.readLine();
-            System.out.println(test);
-
-            while(true){
-                Socket sock = servSock.accept();
-
-                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-
-                servSock.close();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if (".".equals(inputLine)) {
+                    out.println("good bye");
+                    break;
+                }
+                out.println(inputLine);
             }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        try {
+            Server server = new Server();
+            server.start(7000);
+
+            String test = in.readLine();
+            System.out.println(test);
+            
+            servSock.close();
 
         } catch (IOException e) {
             System.err.println(e);
